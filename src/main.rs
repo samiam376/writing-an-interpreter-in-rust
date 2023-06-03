f594ff89fd1a2833620a5062020b72ae6378222f
samiam376 <ssheridan211@gmail.com>
2023-06-02 23:22:59 -0700

    start repl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::io::Write;
+
+use writing_an_interpreter_in_rust::{lexer::Lexer, token::Token};
+
+const PROMPT: &str = ">> ";
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        print!("{}", PROMPT);
+        std::io::stdout().flush().unwrap();
+        let input = &mut String::new();
+        std::io::stdin().read_line(input).unwrap();
+        let mut lexer = Lexer::new(input);
+        loop {
+            let token = lexer.next_token();
+            println!("{:?}", token);
+            if token == Token::EOF {
+                break;
+            }
+        }
+    }
 }
